9fd480b71088ba27796e4dca9d611baf593a21ab
Julien Bongars <julienbongars23@gmail.com>
2023-12-09 11:57:53 +0000

    feat: init front project

--- frontend/components/SimpleDisplayer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SimpleDisplayComponent({ content }) {
-  return <div>{content && <pre>{JSON.stringify(content, null, 2)}</pre>}</div>;
-}
-
-export { SimpleDisplayComponent };
